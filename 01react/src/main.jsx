aab8eeb109c944336a2215d8ce2bae7e05fca0cc
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-08-09 21:45:48 +0530

    using the code in form thats its already compiled for react

--- 01react/src/main.jsx
+++ 01react/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 
+const ReactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'Click me to visit site'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+
+  ReactElement
+
 )
