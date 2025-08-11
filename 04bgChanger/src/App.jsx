871f891e1b4518eaf6279de02a9dced95f43279e
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-08-12 03:07:33 +0530

    completed the bgChanger code

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {useState} from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bgcolor,setbgColor] = useState("Olive")
+  const colors  =["Red","Green","Yellow","Purple"]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='w-screen h-screen' style={{backgroundColor: bgcolor}}></div>
+    <div className='fixed flex flex-wrap bottom-12 px-2 inset-x-0 justify-center'>
+    <div className='flex-wrap gap-3  bg-blue-400 text-white rounded-2xl text-2xl flex'>
+      {/* <button onClick={()=>setColor("red")} className="rounded-xl p-2 m-4 bg-red-500 shadow-xl">Button 1</button>
+      <button className="rounded-xl p-2 m-4 bg-green-500 shadow-xl  " onClick={()=>setColor("green")}>Button 2</button>
+      <button className="rounded-xl p-2 m-4 bg-yellow-500 shadow-xl " onClick={()=>setColor("yellow")}>Button 3</button>
+      <button className="rounded-xl p-2 m-4 bg-purple-500 shadow-xl " onClick={()=>setColor("purple")}>Button 4</button> */}
+  {
+    colors.map((color,index)=>{
+      return(
+        <button  key={index} className={`rounded-xl flex justify-center p-2 m-4 bg-${color.toLowerCase()}-500 shadow-xl min-w-[10vw]`
+         } onClick={()=>setbgColor(color.toLowerCase())}>{color}</button>
+      );
+    })
+  }
+    </div>
+    </div>
     </>
   )
 }
