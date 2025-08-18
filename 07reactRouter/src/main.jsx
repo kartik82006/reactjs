0626be0588f84a6f2d6e2712d3c24ec0d3fede5d
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-08-19 01:32:38 +0530

    router other way

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,34 +1,35 @@
 import ReactDOM from "react-dom/client";
-import { Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import "./index.css";
 import Layout from "./Layout.jsx";
 import {About,Contact} from "./imports.js"; // Importing components from imports.js
 import App from "./App.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    children: [
-      { index: true, element: <App /> },   // Home page at "/"
-      { path: "about", element: <About /> },
-      { path: "contact", element: <Contact /> },
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout/>,
+//     children: [
+//       { index: true, element: <App /> },   // Home page at "/"
+//       { path: "about", element: <About /> },
+//       { path: "contact", element: <Contact /> },
       
-    ],
-  },
-]);
+//     ],
+//   },
+// ]);
 
 // use the below approach only when not using createBrowserRouter
 
-// const router = createBrowserRouter([
-//   <Routes>
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+  
 //     <Route path="/" element={<Layout />}>
 //       <Route index element={<App />} />
 //       <Route path="about" element={<About />} />
 //       <Route path="contact" element={<Contact />} />
 //     </Route>
-//   </Routes>
-// ]);
+//   )
+// );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
