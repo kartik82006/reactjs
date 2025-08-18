82a80b491c7efeb6263c0c0ff177df3866319f66
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-08-18 23:19:36 +0530

    added router dom

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,29 @@
 // import { StrictMode } from 'react'
 import { BrowserRouter } from "react-router";
 import { ReactDOM } from 'react-dom/client'
+import { RouterProvider,createBrowserRouter } from "react-router";
 import './index.css'
 import App from './App.jsx'
 
 const root = document.getElementById("root");
+
+// first way to make a router 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
+    }
+    ]
+  }
+])
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
