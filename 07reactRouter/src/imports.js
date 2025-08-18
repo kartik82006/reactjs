329f4c28fe9c50db56acf0a91f2e02cf67cac1b3
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-08-19 01:24:55 +0530

    connected all routes in navbar

---
+++ 07reactRouter/src/imports.js
@@ -0,0 +1,5 @@
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
+export { About, Header, Footer, Contact };
