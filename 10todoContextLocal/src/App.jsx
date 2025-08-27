0b036b838d6e546445a785418a1efb05fbae1e4f
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-08-27 22:38:32 +0530

    completed todo along with local storage

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts';
+import ToDoForm from './components/ToDoForm';
+import TodoItem from './components/TodoItem';
 
 function App() {
   const [todos,setTodos]= useState([]);
@@ -14,22 +16,37 @@ function App() {
   const deleteTodo =(id)=>{
     setTodos((prev)=> prev.filter((todo)=> todo.id!== id))
   }
-  const toggleComplete= (id) =>{
+  const toggleComplete = (id) => {
     setTodos((prev)=> prev.map((prevTodo)=> (prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo)))
   }
 
-  return (
-    <TodoProvider value={{todos,addTodo,updateTodo,toggleComplete,deleteTodo}}>
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem('todos'))
+    if(todos && todos.length>0){
+      setTodos(todos)
+    }
+  },[])
 
-      
+  useEffect(()=>{
+    localStorage.setItem('todos', JSON.stringify(todos))
+  },[todos])
+
+  return (
+    <TodoProvider value={{todos,addTodo,updateTodo,toggleComplete,deleteTodo}}> 
      <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        <ToDoForm /> 
                     </div>
                     <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
+                        {
+                          todos.map((todo)=>(
+                            <div className='w-full' key={todo.id}>
+                              <TodoItem todo={todo} />
+                            </div>
+                          ))
+                        }
                     </div>
                 </div>
             </div>
---
+++ 10todoContextLocal/src/components/index.js
@@ -0,0 +1,3 @@
+import {TodoItem} from './TodoItem';
+import {ToDoForm} from './ToDoForm';
+export default {ToDoForm, TodoItem};
