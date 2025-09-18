a3235a789414a1254c92c2bad9a7865b185c9b33
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2025-09-18 16:36:16 +0530

    configured Store

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import process from  'process';
+import {useState,useEffect} from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import {useDispatch} from 'react-redux';
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice';
+
+
 
 function App() {
-  
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <div>
-        <h1>Welcome to the Blog</h1>
-      </div>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+    .finally(()=>{setLoading(false)});
+  },[])
+
+  return !loading  ?  (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className="w-full block">
+      <Header/>
+      <Footer/>
+    </div>
+  </div>
+) : (null);
 }
 
 export default App
